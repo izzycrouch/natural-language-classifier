6270024936ddffb1809ad940a4ee741ac6e35866
izzycrouch <belle.crouch00@gmail.com>
2025-11-27 09:11:12 +0000

    Feat: Change functions in train_model.py to be part of a TrainModel class to fix issue with multiple vectorizers.

--- classifier_layer/emotion_classifier.py
+++ classifier_layer/emotion_classifier.py
@@ -1,4 +1,31 @@
+from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 
-with open('./trained_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+class EmotionClassifier:
+    def __init__(self):
+        with open('./trained_model.pkl', 'rb') as f:
+            ss = pickle.load(f)
+            print(ss['model'])
+        
+    #     self.label_map = {
+    #         0: 'sadness',
+    #         1: 'joy',
+    #         2: 'love',
+    #         3: 'anger',
+    #         4: 'fear',
+    #         5: 'surprise'
+    #     }
+
+    #     self.vectorizer = CountVectorizer()
+
+    # def classify_emotion(self, input_text:str):
+        
+    #     input_text_vectors = self.vectorizer.fit_transform([input_text])
+        
+    #     predicted_label = self.model.predict(input_text_vectors)
+    #     emotion = self.label_map.get(predicted_label, )
+        # return emotion
+        
+emotions = EmotionClassifier()
+# emotion = emotions.classify_emotion(input_text='I am feeling sad.')
+print(emotions)
--- train_layer/train_model.py
+++ train_layer/train_model.py
@@ -5,74 +5,86 @@ import json
 import pickle
 import logging
 
-def get_data(input_path):
-    with open (input_path, mode='r') as f:
-        data = json.load(f)
-    return data
+logging.basicConfig(filename='./model_logger.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s: %(levelname)s: %(message)s') 
 
-
-def train_model(training_data):
-
-    X_train = [i['text'] for i in train_data]
-    y_train = [i['label'] for i in train_data]
-
-    vectorizer = CountVectorizer()
-    X_train_vectors = vectorizer.fit_transform(X_train)
-
-    model = MultinomialNB()
-
-    model.fit(X_train_vectors, y_train)
-
-    return model
-
-
-def test_model(model, test_data):
-
-    X_test = [i['text'] for i in train_data]
-    y_test = [i['label'] for i in train_data]
+class TrainModel:
+    def __init__(self):
+        self.vectorizer = CountVectorizer()
+        self.model = MultinomialNB()
+        self.logger = logging.getLogger(__name__)
     
-    vectorizer = CountVectorizer()
-    X_test_vectors = vectorizer.fit_transform(X_test)
 
-    y_pred = model.predict(X_test_vectors)
-
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='weighted')
-    recall = recall_score(y_test, y_pred, average='weighted')
-    f1 = f1_score(y_test, y_pred, average='weighted')
-    confusion = confusion_matrix(y_test, y_pred)
-    class_report = classification_report(y_test, y_pred, labels=[0,1, 2, 3, 4, 5])
-
-    print(f'Accuracy score: {accuracy}')
-    print(f'Precision score: {precision}')
-    print(f'Recall score: {recall}')
-    print(f'F1 score: {f1}')
-
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='./model_logger.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s: %(levelname)s: %(message)s')  
-    logger.info(f'Model has been tested with data.\nConfusion Matrix: \n{confusion}\n Classification Report: \n{class_report}\n')
-
-    print('The confusion matrix and classification report for the tested data has been logged.')
+    def get_data(self, input_path):
+        with open (input_path, mode='r') as f:
+            data = json.load(f)
+        return data
 
 
-def save_trained_model(model):
-    with open('trained_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
-    print('Trained model has been saved.')
+    def train_model(self, training_data):
 
+        X_train = [i['text'] for i in training_data]
+        y_train = [i['label'] for i in training_data]
+
+        X_train_vectors = self.vectorizer.fit_transform(X_train)
+
+        self.model.fit(X_train_vectors, y_train)
+
+        return self.model
+
+
+    def test_model(self, test_data):
+
+        X_test = [i['text'] for i in test_data]
+        y_test = [i['label'] for i in test_data]
+        
+        X_test_vectors = self.vectorizer.transform(X_test)
+
+        y_pred = self.model.predict(X_test_vectors)
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred, average='weighted')
+        recall = recall_score(y_test, y_pred, average='weighted')
+        f1 = f1_score(y_test, y_pred, average='weighted')
+        confusion = confusion_matrix(y_test, y_pred)
+        class_report = classification_report(y_test, y_pred, labels=[0,1, 2, 3, 4, 5])
+
+        print(f'Accuracy score: {accuracy}')
+        print(f'Precision score: {precision}')
+        print(f'Recall score: {recall}')
+        print(f'F1 score: {f1}')
+
+        self.logger.info(f'Model has been tested with data.\nConfusion Matrix: \n{confusion}\n Classification Report: \n{class_report}\n')
+
+        print('The confusion matrix and classification report for the tested data has been logged.')
+
+
+    def save_trained_model(self):
+        with open('trained_model.pkl', 'wb') as f:
+            pickle.dump(self.model, f)
+        print('Trained model has been saved.')
+
+    def load_trained_model(self):
+        with open('trained_model.pkl', 'rb') as f:
+            model = pickle.load(f)
+            self.model = model
+        print('Trained model has been loaded.')
+        return self.model
+
+
+train_emotion_model = TrainModel()
 
 train_data_path = './ingest_layer/emotion_train.json'
-train_data = get_data(train_data_path)
+train_data = train_emotion_model.get_data(train_data_path)
 
-model = train_model(train_data)
+trained_model = train_emotion_model.train_model(train_data)
 
 test_data_path = './ingest_layer/emotion_test.json'
-test_data = get_data(test_data_path)
+test_data = train_emotion_model.get_data(test_data_path)
 
-test_model(model, test_data)
+train_emotion_model.test_model(test_data)
 
 validation_data_path = './ingest_layer/emotion_validation.json'
-validation_data = get_data(validation_data_path)
-test_model(model, validation_data)
+validation_data = train_emotion_model.get_data(validation_data_path)
+train_emotion_model.test_model(validation_data)
 
-save_trained_model(model)
+train_emotion_model.save_trained_model()
