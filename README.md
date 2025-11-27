3b2f87605df19f17ebccf12bf82e71430bf1613d
izzycrouch <belle.crouch00@gmail.com>
2025-11-27 17:15:57 +0000

    Feat: Update README.md and change interface function to work with the NewsClassifier.

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ In this project I will build a full AI pipeline, using a publically accessable d
 3. Run 'pip install datasets pytest scikit-learn transformers accelerate torch' (See requirment.txt for more information.)
 4. Run 'python3 ingest_layer/ingest_data.py' to load and clean data.
 5. Run 'pytest tests/test_ingest_data.py' to check that ingest layer passes tests.
-6. Run 'python3 train_layer/train_random_forest_model.py' to train classifier model.
-7. Run 'pytest tests/test_emotion_classifier' to check that EmotionClassifier class passes tests.
-8. Run 'python3 classifier_layer/simple_interface.py' to predict emotions through the CLI.
+6. Run 'python3 train_layer/train_news_model.py' to train classifier model.
+7. Run 'pytest tests/test_news_classifier' to check that NewsClassifier class passes tests.
+8. Run 'python3 classifier_layer/simple_interface.py' to predict news topics through the CLI.
 
 ### Citation
 
--- classifier_layer/simple_interface.py
+++ classifier_layer/simple_interface.py
@@ -1,18 +1,18 @@
-# from emotion_classifier import EmotionClassifier
+from news_classifier import NewsClassifier
 
-# def interface():
-#     print('\nWelcome to Emotion Classifier! \n\Input some text (then press enter) and I will evaluate what emotion it classifies under. To exit, type exit and press enter.')
-#     while True:
-#         input_string = input('\nEnter a sentence to classify: ')
+def interface():
+    print('\nWelcome to Emotion Classifier! \n\Input some text (then press enter) and I will evaluate what emotion it classifies under. To exit, type exit and press enter.')
+    while True:
+        input_string = input('\nEnter a sentence to classify: ')
         
-#         if input_string.casefold() != 'exit':
+        if input_string.casefold() != 'exit':
             
-#             emotion_classifier = EmotionClassifier('./models/trained_classifier_model.pkl')
-#             emotion = emotion_classifier.classify_emotion(input_text=input_string)
-#             print(f'\nInput: {input_string} \nClassified Emotion: {emotion.capitalize()}')
+            news_classifier = NewsClassifier('./models/trained_classifier_model.pkl')
+            topic = news_classifier.classify_news(input_text=input_string)
+            print(f'\nInput: {input_string} \nClassified Topic: {topic.capitalize()}')
         
-#         else:
-#             print('\nGoodbye!')
-#             break
+        else:
+            print('\nGoodbye!')
+            break
 
-# interface()
+interface()
